53356bceb389bc9df6f1451a9c6daf023a97026b
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2024-12-17 14:57:02 -0300

    Tabelas SQL
    A tabela é projetada para armazenar informações sobre eventos processados, e cada linha na tabela representa um único evento.

---
+++ processed_events.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS processed_events (
+    ip VARCHAR,
+    event_timestamp TIMESTAMP(3),
+    referrer VARCHAR,
+    host VARCHAR,
+    url VARCHAR,
+    geodata VARCHAR
+);
